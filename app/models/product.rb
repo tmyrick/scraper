f85518d309be204c45bc388a495b73aee600bc8a
Richard McSharry <Richard McSharry>
2017-09-04 20:20:11 +0200

    Adds validation and simple form layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+
+  validate :asin_or_url
+
+  private
+
+  def asin_or_url
+    unless amazon_asin.blank? ^ amazon_url.blank?
+      errors.add(:base, "Specify the URL or ASIN, not both")
+    end
+  end
 end
